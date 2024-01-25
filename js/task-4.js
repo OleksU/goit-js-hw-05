7160a4766114ed185bc5a066b771f52acf2a367e
OleksU <cibulskiy089@gmail.com>
2024-01-25 03:22:42 +0100

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -11,3 +11,52 @@ const getTotalBalanceByGender = (users, gender) => {
     }
     return totalBalance;
 }
+
+// const getTotalBalanceByGender = (users, gender) => {
+//      const totalBalance = users.filter(item => {
+//         item.gender.includes(gender)}).reduce((total, num) => {
+//         return total + num.balance;},0);
+//      return totalBalance;
+// }
+
+const allUsers = [
+    {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(allUsers, "male"));
+
+console.log(getTotalBalanceByGender(allUsers, "female"));
